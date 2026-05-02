a00862736d4d29e5117fa1195b7067d730df800e
Mandeindu <130290577+Mandeindu@users.noreply.github.com>
2026-05-02 18:29:09 +0530

    Add defect prediction function in predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,1 +1,15 @@
+from model.model_loader import load_model
+from app.preprocess import preprocess_image
+import numpy as np
 
+model = load_model()
+
+def predict_defect(image_path):
+    img = preprocess_image(image_path)
+
+    prediction = model.predict(img)
+
+    if prediction[0][0] > 0.5:
+        return "Defective PCB"
+    else:
+        return "Good PCB"
