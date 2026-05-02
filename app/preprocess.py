cd2bc4aa81e3fdb450c351f54d37b1b55e00c682
Mandeindu <130290577+Mandeindu@users.noreply.github.com>
2026-05-02 18:28:47 +0530

    Update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,1 +1,14 @@
+import tensorflow as tf
+import numpy as np
 
+def preprocess_image(img_path):
+    img = tf.keras.preprocessing.image.load_img(
+        img_path,
+        target_size=(224,224)
+    )
+
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+
+    return img_array
