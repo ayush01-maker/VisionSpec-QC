82d81cd19f657e90c57179d9c1122192c57ccde4
Mandeindu <130290577+Mandeindu@users.noreply.github.com>
2026-05-02 18:29:31 +0530

    Implement Flask API for defect prediction

--- app/app.py
+++ app/app.py
@@ -1,1 +1,28 @@
+from flask import Flask, request, jsonify
+from predict import predict_defect
+import os
 
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/")
+def home():
+    return "VisionSpec-QC API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files["image"]
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
+    result = predict_defect(file_path)
+
+    return jsonify({
+        "prediction": result
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
