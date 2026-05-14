efeb16c33b8c019e3977076e5861a0eac02cc098
Mandeindu <130290577+Mandeindu@users.noreply.github.com>
2026-05-14 18:49:11 +0530

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,24 +1,35 @@
-from model.model_loader import load_model
-from app.preprocess import preprocess_image
 import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
 
-model = load_model()
+MODEL_PATH = "../model/saved_model/model.keras"
 
-def predict_defect(image_path):
-    img = preprocess_image(image_path)
+model = load_model(MODEL_PATH)
 
-    prediction = model.predict(img)
+CLASS_NAMES = ["Defective", "Non-Defective"]
 
-    if prediction[0][0] > 0.5:
-        return "Defective PCB"
-    else:
-        return "Good PCB"
 
-from app.gradcam import save_gradcam
+def preprocess(img):
 
-save_gradcam(
-    model,
-    img,
-    image_path,
-    "uploads/gradcam_result.jpg"
-)
+    img = img.resize((224, 224))
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    return img_array
+
+
+def predict_image(uploaded_file):
+
+    img = Image.open(uploaded_file).convert("RGB")
+
+    processed = preprocess(img)
+
+    prediction = model.predict(processed)
+
+    predicted_class = np.argmax(prediction)
+
+    confidence = np.max(prediction) * 100
+
+    return CLASS_NAMES[predicted_class], confidence, img
